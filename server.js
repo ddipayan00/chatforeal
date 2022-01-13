a0d08d877a459d46051a49e6c0adcfc806760280
ddipayan00 <47278363+ddipayan00@users.noreply.github.com>
2022-01-14 00:53:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ io.on('connection', (socket) => {
     })
     socket.on('join-room',(room,cb)=>{
           socket.join(room);
-          cb(`you joined ${room}`)
+          cb(`you joined Global Room`)
     })
 
 })
